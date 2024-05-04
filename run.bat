e86019c7c3d240635bc35fdccb3b96e7b4e07b5d
sohaib <70706835+sdev15@users.noreply.github.com>
2024-05-04 12:50:37 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,15 +1,15 @@
 @echo off
 cls
 
-:: Display "username checker" in bold font for five seconds
+
 echo. & echo. & echo. & echo. & echo. & echo. & echo.
 echo                   username checker
 timeout /t 5 >nul
 
-:: Clear the screen after five seconds
+
 cls
 
-:: Display "BY M5TL" in fancy bold font for five seconds
+
 echo. & echo. & echo. & echo. & echo. & echo. & echo.
 echo
 echo  _                    ____  _   _ 
@@ -21,14 +21,14 @@ echo        |___/
 
 timeout /t 5 >nul
 
-:: Simple loading bar
+
 echo.
 echo [#############       ] 50%
 timeout /t 2 >nul
 
-:: Clear the screen after loading
+
 cls
 
-:: Run py.py
+
 echo Running the tool
 python py.py
